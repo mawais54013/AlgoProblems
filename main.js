751149df7a18c349bccae580136d463137cdd23f
MAwais <mawais54013@outlook.com>
2019-03-07 20:09:52 -0800

    tested version

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 var solutions = ['images/img1.png'];
 
-var titles = ['FizzBuzz Problem']
+var titles = ['FizzBuzz Problem'];
+
+var time = ['O(n)'];
 
 function problem(data,num)
 {
@@ -12,4 +14,33 @@ function problem(data,num)
 let pickedItem = localStorage.getItem('pickedProb');
 let pickedNumber = localStorage.getItem('pickedNum');
 
-console.log(pickedItem , pickedNumber);
+// console.log(pickedItem , pickedNumber);
+
+let mainTitle = '';
+let number = '';
+let fixPic = '';
+
+$("#button1").click(function(){
+    for(let i = 0; i <= solutions.length; i++)
+    {
+        if(i == pickedNumber)
+        {
+            fixPic = solutions[i];
+            number = time[i];
+            mainTitle = titles[i];
+        }
+    }
+    
+    $('#mainContent').append(`
+        <div class="row">
+            <div class="col-lg-12 text-center">
+                <img src="${fixPic}">
+                <h1>${mainTitle}</h1>
+                <p>${number}</p>
+            </div>
+        </div>
+    `);
+});
+
+
+
